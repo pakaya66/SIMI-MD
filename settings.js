19a4a42cb96f101bf2d1e943196df4e37c0afcaf
pakaya66 <lakshithadilmina@gmail.com>
2025-03-08 13:44:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false //auto kick spammer (true to on, false to off)
 global.autoswview = true //auto status/story view
-global.antiswview = false //auto status/story view
+global.antiswview = true //auto status/story view
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
